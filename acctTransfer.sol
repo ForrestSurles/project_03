f124c06d3ec97897b18b82a960aa1927d27dabcd
John Weldon <85202691+jpweldon@users.noreply.github.com>
2021-11-22 02:21:12 -0600

    Update Smart Contract

--- acctTransfer.sol
+++ acctTransfer.sol
@@ -3,56 +3,69 @@ pragma solidity ^0.5.5;
 // Define a new contract named `AccountTransfer`
 contract AccountTransfer {
 
-    address payable firmAcct;
     address payable ethAcct;
     address payable mktAcct;
-    uint128 public mktBalance;
-    uint128 public ethBalance;
-    uint128 public fees;        // TODO: Calculation and passing in of the fees
+    address payable firmAcct;
+    uint public ethBalance;
+    uint public mktBalance;
+    uint public totalRequired;
+    uint public gasFee = 133; //133 GWEI
+    uint public conversionFee = 11000000; //0.011 ETH = 11000000 GWEI
+    uint public fees = gasFee + conversionFee;
     
-    // Question: How does the program know where to transfer from; how do we code it?
-    // Question: Python Streamlit application to call solidity application. How to call, compile, and deploy solidity file?
+    // TODO: Python Streamlit application to call solidity application. Call, compile, and deploy solidity file.
+    
+    /*
+    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
+    */
+    function setAccounts() public{
 
-    function ethToMkt(uint amount, uint128 gasFee, uint128 transferFee) public {
+        // Set the values of `mktAcct` and `firmAcct`
+        //TODO: mktAcct = wallet address; //input wallet address
+        //TODO: firmAcct = wallet address; //input wallet address
+    }
+
+    function ethToMkt(uint amount) public {
+
+        //Calculate totalRequired
+        totalRequired = amount + fees
 
         // Verify account balance capable of payment
-        require(ethAcct.balance >= amount + fees, "Insufficient funds.");
+        require(ethBalance >= totalRequired, "Insufficient funds.");
         
-        //Transfer transaction fee
-        gasFee = 133;             //133 GWEI
-        transferFee = 11000000;        //0.011 ETH = 11000000 GWEI
-        fees = gasFee + transferFee;
-        
-        // Call the `transfer` function of the `ethAcct` and pass it the `transferAmount` to transfer as an argument.
-        transferAmount = amount - fees;
-        mktAcct.transfer(transferAmount);
+        // Call the `transfer` function of the `mktAcct` and pass it the `amount` to transfer as an argument.
+        mktAcct.transfer(amount);
         
         // Call the `transfer` function of the `firmAcct` and pass it the `fees` to transfer as an argument.
         firmAcct.transfer(fees);
 
-        // Transfer from the ethereum acct to the market account
+        // Call the `mktBalance` variable and set it equal to the balance of the `mktAcct.balance`
         mktBalance = mktAcct.balance;
+        
+        // Call the `ethBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        ethBalance = address(this).balance;
         
     }
 
-    function mktToEth(uint amount, uint128 gasFee, uint128 transferFee) public {
+    function mktToEth(uint amount) public {
+
+        //Calculate totalRequired
+        totalRequired = amount + fees
 
         // Verify account balance capable of payment
-        require(mktAcct.balance >= amount + fees, "Insufficient funds.");
+        require(mktBalance >= totalRequired, "Insufficient funds.");
         
-        //Transfer transaction fee
-        gasFee = 133;             //133 GWEI
-        transferFee = 11000000;        //0.011 ETH = 11000000 GWEI
-        fees = gasFee + transferFee;
-        
-        // Call the `transfer` function of the `ethAcct` and pass it the `transferAmount` to transfer as an argument.
-        transferAmount = amount - fees;
-        ethAcct.transfer(transferAmount);
+        // Call the `transfer` function of the `ethAcct` and pass it the `amount` to transfer as an argument.
+        //TODO: ethAcct = address(this)
+        //TODO: ethAcct.transfer(totalRequired); //Pull money from mktBalance. Check whether the Deposit function can be used here.
         
         // Call the `transfer` function of the `firmAcct` and pass it the `fees` to transfer as an argument.
         firmAcct.transfer(fees);
 
-        // Transfer from the ethereum acct to the market account
-        ethBalance = ethAcct.balance;
+        // Call the `mktBalance` variable and set it equal to the balance of the `mktAcct.balance`
+        mktBalance = mktAcct.balance;
+        
+        // Call the `ethBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        ethBalance = address(this).balance;
         
     }
