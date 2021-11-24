931d4c7d26a7466db43fc232de17632922aaa740
Forrest Surles <85535986+ForrestSurles@users.noreply.github.com>
2021-11-24 16:48:44 -0600

    Fix env calls and clean more code

--- app.py
+++ app.py
@@ -20,35 +20,40 @@ sidebar_msg += f"- John P Weldon\n"
 sidebar_msg += f"- Ashley Guidot\n"
 sidebar_msg += f"- Forrest Surles\n"
 sidebar_msg += f"- Vishwanath Subramanian"
-
 st.sidebar.write(sidebar_msg)
+
 st.image('./cover_image.jpeg', width=700)
 st.markdown("### Smart Risk Movements")
-st.markdown("This application assists client portfolio management by calculating transaction fees associated with transferring between Ethereum and a fund before enabling the client to initiate the transfer. The application then automates the transfer of the amount between Etherum and a fund.")
+
+desc_msg = f'This application assists client portfolio management by '
+desc_msg += f'calculating transaction fees associated with transferring '
+desc_msg += f'between ETH and market accounts '
+desc_msg += f'before then initiating the transfer.\n\n'
+st.markdown(desc_msg)
 
 # query acct address / print input verification
-address_one = st.text_input("Market Account Address:")
-st.write(address_one)
+address_mkt = st.text_input("Market Account Address:")
+st.write(address_mkt)
 
 # query acct private key for processing
-priv_key = st.text_input("Enter your Private Key:", type="password")
+mkt_acct_key = st.text_input("Enter Private Key:", type="password")
 
-# amount of ether to transfer 
+# query amount of ether to transfer 
 amount_transfer = Web3.toWei(
         Decimal(st.number_input("How much would you like to transfer?:")),
         'ether'
 )
 
-# select acct
+# query transfer source acct
 options = st.selectbox(
         "Which account would you like to transfer from?",
         ["Market Account", "Ethereum Account"]
 )
 
-# declare transaction fees
+# state transaction fees
 st.markdown("The fee associated with this transaction is 55,000 GWEI.")
 
-# clarify intent from user
+# query clarification of intent
 fee_agreement = st.selectbox(
         "Would you like to proceed with transaction?",
         ["Yes", "No"]
@@ -60,7 +65,10 @@ fee_agreement = st.selectbox(
 
 # Load environment variables
 load_dotenv()
-PRIVATE_KEY_ONE = os.getenv("PRIVATE_KEY_1")
+PRIVATE_KEY_FIRM = os.getenv("PRIVATE_KEY_FIRM")
+
+# provide firm wallet address to receive transaction fee
+address_firm = '0xb1fB7f3c3D78DcB6B68D07ade463ca3Cd63fB373'
 
 # smart contract bytecode/abi for local execution
 bytecode = json.load(open('bytecode.json'))['object']
@@ -75,52 +83,57 @@ strategy = construct_time_based_gas_price_strategy(15)
 # set gas price
 w3.eth.setGasPriceStrategy(strategy)
 
-our_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+deployed_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
 
-
-# provide firm wallet address to receive transaction fee
-address_two = '0xb1fB7f3c3D78DcB6B68D07ade463ca3Cd63fB373'
-
-# address two private key
-PRIVATE_KEY_TWO = os.getenv("PRIVATE_KEY_2")
 
 # if user agrees to fees and executes proceed to further if else statement
 if fee_agreement == 'Yes' and st.button('Execute'):
 
-        # Getting nonce
-        # Using toHex for easier visability when deployed
-        gasPrice = 55000 #gwei
+        # gas price ceiling for transaction (in wei)
+        gasPrice = 55000 
 
+        # direction of transfer logic
         if options == "Market Account":
 
-                nonce = Web3.toHex(w3.eth.getTransactionCount(address_one))
+                # retrieve nonce for originating acct
+                nonce = Web3.toHex(w3.eth.getTransactionCount(address_mkt))
 
-                tr = {'to': address_two, 
-                        'from': address_one,
+                # set transaction parameters
+                tr = {'to': address_firm, 
+                        'from': address_mkt,
                         'value': Web3.toHex(amount_transfer), 
                         'gasPrice': Web3.toHex(gasPrice), 
                         'nonce': nonce,
                         'data': "0x" + bytecode,
                         'gas': 5000000,
                         }
-                signed = w3.eth.account.sign_transaction(tr, priv_key)
+                
+                # validate transaction with private key
+                signed = w3.eth.account.sign_transaction(tr, mkt_acct_key)
         else:
 
-                nonce = Web3.toHex(w3.eth.getTransactionCount(address_two))
+                # retrieve nonce for originating acct
+                nonce = Web3.toHex(w3.eth.getTransactionCount(address_firm))
 
-                tr = {'to': address_one, 
-                        'from': address_two,
+                # set transaction parameters
+                tr = {'to': address_mkt, 
+                        'from': address_firm,
                         'value': Web3.toHex(amount_transfer), 
                         'gasPrice': Web3.toHex(gasPrice), 
                         'nonce': nonce,
                         'data': "0x" + bytecode,
                         'gas': 5000000,
                         }
-                signed = w3.eth.account.sign_transaction(tr, PRIVATE_KEY_TWO)
+
+                # validate transaction with private key
+                signed = w3.eth.account.sign_transaction(tr, PRIVATE_KEY_FIRM)
         
+        # initiate transfer
         tx = w3.eth.sendRawTransaction(signed.rawTransaction)    
         tx_receipt = w3.eth.waitForTransactionReceipt(tx)
 
-        # Complete transaction
-        our_contract = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+        # update contract with new balance 
+        deployed_contract = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+        # declare completion
         st.write('Transaction Complete.')
