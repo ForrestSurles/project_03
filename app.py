dbf31aba5c814e7664feec3093a3bc5491641ad1
Forrest Surles <85535986+ForrestSurles@users.noreply.github.com>
2021-11-23 22:37:28 -0600

    Write compatible transaction execution logic

--- app.py
+++ app.py
@@ -26,15 +26,12 @@ bytecode = json.load(open('bytecode.json'))['object']
 # Copy abi from Remix
 abi = json.load(open('abi.json'))
 
-# connect to blockchain network
-ganache_url = "http://127.0.0.1:7545"
-w3 = Web3(Web3.HTTPProvider(ganache_url))
 
 # query input from user to initiate transaction
 
 # wallet address
-mkt_account_input = st.text_input("Enter your Market Account Address:")
-st.write(mkt_account_input)
+address_one= st.text_input("Enter your Market Account Address:")
+st.write(address_one)
 
 # private key
 priv_key_input = st.text_input("Enter your Private Key:", type="password")
@@ -49,3 +46,64 @@ st.markdown("The fee associated with this transaction is 21,000 GWEI.")
 # clarify intent from user
 fee_agreement = st.selectbox("Would you like to proceed with transaction?",
         ["Yes", "No"])
+
+
+# execute transaction with user input
+
+# connect to blockchain network
+ganache_url = "http://127.0.0.1:7545"
+w3 = Web3(Web3.HTTPProvider(ganache_url))
+
+# web3 function that allows us to set the amount of time we want transactions to take (in secs)
+strategy = construct_time_based_gas_price_strategy(15)
+# Sets the gas price
+w3.eth.setGasPriceStrategy(strategy)
+
+our_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+
+# provide firm wallet address to receive transaction fee
+address_two = '0x2CAf7eC7Fe79599C0F2a68b566B52F97D13ff7dD'
+
+# address two private key
+PRIVATE_KEY_TWO = os.getenv("PRIVATE_KEY_2")
+
+if fee_agreement == 'Yes' and st.button('Execute'):
+
+        # Getting nonce
+        # Using toHex for easier visability when deployed
+        gasPrice = 21000 #gwei
+
+        if options == "Market Account":
+
+                nonce = Web3.toHex(w3.eth.getTransactionCount(address_one))
+
+                tr = {'to': address_two, 
+                        'from': address_one,
+                        'value': Web3.toHex(0), 
+                        'gasPrice': Web3.toHex(gasPrice), 
+                        'nonce': nonce,
+                        'data': "0x" + bytecode,
+                        'gas': 5000000,
+                        }
+                signed = w3.eth.account.sign_transaction(tr, priv_key_input)
+        else:
+
+                nonce = Web3.toHex(w3.eth.getTransactionCount(address_two))
+
+                tr = {'to': address_one, 
+                        'from': address_two,
+                        'value': Web3.toHex(0), 
+                        'gasPrice': Web3.toHex(gasPrice), 
+                        'nonce': nonce,
+                        'data': "0x" + bytecode,
+                        'gas': 5000000,
+                        }
+                signed = w3.eth.account.sign_transaction(tr, PRIVATE_KEY_TWO)
+        
+        tx = w3.eth.sendRawTransaction(signed.rawTransaction)    
+        tx_receipt = w3.eth.waitForTransactionReceipt(tx)
+
+        # TODO: I need to change the name to our contract name
+        our_contract = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+        st.write('Transaction Complete.')
